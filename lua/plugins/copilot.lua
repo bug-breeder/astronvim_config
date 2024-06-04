48cbd59dc6a83eaffd178012099045f49bee15e9
Nguyen Tran Viet Anh <anh.ntv@teko.vn>
2024-06-04 15:12:44 +0700

    add lsp-signature, copilot

---
+++ lua/plugins/copilot.lua
@@ -0,0 +1,22 @@
+---@type LazySpec
+return {
+  "zbirenbaum/copilot.lua",
+  cmd = "Copilot",
+  event = "InsertEnter",
+  opts = {
+    suggestion = {
+      enabled = true,
+      auto_trigger = true,
+      debounce = 150,
+      keymap = {
+        -- accept = "<M-;>",
+        accept = "<Right>",
+        accept_word = false,
+        accept_line = false,
+        next = "<M-]>",
+        prev = "<M-[>",
+        dismiss = "<C-]>",
+      },
+    },
+  },
+}
